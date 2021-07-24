07fdd956cb18d0b6d18005fe2a35f3366c452301
Dexshine <b_o_y_s@hotmail.com>
2021-07-25 00:37:05 +0700

    1.3

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function pressEnter(event){
     const x = event.key
     if(x === 'Enter') clickCallback() //1.2) if Enter was pressed, call clickCallback
 }
+const postContainer = document.createElement('div')
+postContainer.setAttribute('id', 'postContainer')
 //when add task button clicked
 const clickCallback = () => {
     if(inputTask.value === ''){ //check if input is empty
@@ -12,14 +14,13 @@ const clickCallback = () => {
     }
     else{
         console.log(inputTask.value)
-        const postContainer = document.createElement('div')
         const div1 = document.createElement('div')
         const p1 = document.createElement('p')
-        const allDiv = postContainer.querySelectorAll('div')
         p1.innerHTML = inputTask.value
-        document.body.append(postContainer)
-        postContainer.append(div1)
-        div1.append(p1)
+        document.body.append(postContainer)//add post container to HTML
+
+        postContainer.prepend(div1) //add new div ontop of old div
+        div1.append(p1) //add p in new div
 
     } 
 }
