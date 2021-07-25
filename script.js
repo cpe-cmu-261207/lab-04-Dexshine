6f4f16dd4ea762003e59aff2d1a450528d178696
Dexshine <b_o_y_s@hotmail.com>
2021-07-25 22:38:01 +0700

    delete value from localStorage

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const inputTask = document.querySelector('#inputTask')
-//const storedPost = localStorage.getItem('postContainer')
-//const storedDone = localStorage.getItem('doneContainer')
 
 
 //add task by press Enter
@@ -8,19 +6,66 @@ function pressEnter(event){
     const x = event.key
     if(x === 'Enter') clickCallback() //1.2) if Enter was pressed, call clickCallback
 }
-/*const mouseIn = () => {
-    
-}
-const mouseOut = (div1) => {
-    
-}*/
+
 //create main div
 const postContainer = document.createElement('div')
 postContainer.setAttribute('id', 'postContainer')
-//postContainer.innerHTML = storedPost
 const doneContainer = document.createElement('div')
 doneContainer.setAttribute('id', 'doneContainer')
-//doneContainer.innerHTML = storedDone
+//document.body.append(postContainer)//add post container to HTML
+//document.body.append(doneContainer)//add done container below postcontainer
+let i = localStorage.length+1
+const indexPost = []
+const indexDone = []
+postContainer.addEventListener("load", () => {
+    for(let m=0; m<indexPost.length; m++){
+        const div1 = document.createElement('div')
+        div1.setAttribute('class', 'flex w-130 mx-auto')
+        div1.setAttribute('id','box')
+        const p1 = document.createElement('p')
+        p1.setAttribute('class', 'bg-blue-100 mx-auto mr-0 text-2xl w-96 overflow-hidden')
+        p1.innerHTML = localStorage.getItem(m)
+        postContainer.prepend(div1) //add new div ontop of old div
+        const doneButton = document.createElement('button')
+        doneButton.setAttribute('id','done')
+        doneButton.setAttribute('class', 'bg-green-400 mx-auto ml-0 mr-0')
+        doneButton.innerHTML = 'DONE'
+        div1.append(doneButton)
+        const deleteButton = document.createElement('button')
+        deleteButton.setAttribute('id', 'delete')
+        deleteButton.setAttribute('class', 'bg-red-300 mx-auto ml-0')
+        deleteButton.innerHTML = 'DELETE'
+        div1.append(deleteButton)
+        div1.append(p1) //add p in new div
+        document.body.append(postContainer)
+}})
+
+    
+doneContainer.addEventListener("load", () => {
+    for(let n=0; n<indexDone.length; n++){
+        const div1 = document.createElement('div')
+        div1.setAttribute('class', 'flex w-130 mx-auto')
+        div1.setAttribute('id','box')
+        const p1 = document.createElement('p')
+        p1.setAttribute('class', 'bg-blue-100 mx-auto mr-0 text-2xl w-96')
+        p1.innerHTML = '<del>' + localStorage.getItem[n] + '</del'
+        doneContainer.prepend(div1)
+        div1.append(p1)
+        const doneBtn = document.createElement('button')
+        doneBtn.setAttribute('id','done2')
+        doneBtn.setAttribute('class', 'bg-white mx-auto ml-0 mr-0')
+        doneBtn.innerHTML = ''
+        div1.append(doneBtn)
+        const delBtn = document.createElement('button')
+        delBtn.setAttribute('id', 'delete2')
+        delBtn.setAttribute('class', 'bg-white mx-auto ml-0')
+        delBtn.innerHTML = ''
+        div1.append(delBtn)
+        doneContainer.prepend(div1)
+        document.body.prepend(doneContainer)
+    }
+})          
+
 
 
 //when add task button clicked
@@ -29,20 +74,21 @@ const clickCallback = () => {
         alert('Task cannot be empty') //1.4
     }
     else{
-        console.log(inputTask.value)
         const div1 = document.createElement('div')
         div1.setAttribute('class', 'flex w-130 mx-auto')
         div1.setAttribute('id','box')
-        //div1.setAttribute('onmouseenter', 'mouseIn()')
-        //div1.setAttribute('onmouseleave', 'mouseOut()')
         const p1 = document.createElement('p')
         p1.setAttribute('class', 'bg-blue-100 mx-auto mr-0 text-2xl w-96 overflow-hidden')
         p1.innerHTML = inputTask.value
         document.body.append(postContainer)//add post container to HTML
         document.body.append(doneContainer)//add done container below postcontainer
+        inputTask.innerText = ''
         postContainer.prepend(div1) //add new div ontop of old div
         div1.append(p1) //add p in new div
-        //saveToLocalStorage()
+        ++i
+        localStorage.setItem(i,inputTask.value)
+        indexPost.push(localStorage.key(i))
+
         //done button
         const doneButton = document.createElement('button')
         doneButton.setAttribute('id','done')
@@ -53,6 +99,11 @@ const clickCallback = () => {
             postContainer.removeChild(div1)
             p1.setAttribute('class', 'bg-blue-100 mx-auto mr-0 text-2xl w-96')
             p1.innerHTML = '<del>' + p1.innerHTML + '</del'
+            for(let l=0; l<localStorage.length; l++){
+                const tempIndex = localStorage.key(l)
+                const tempValue = localStorage.getItem(tempIndex)
+                if(tempValue == p1.innerHTML) indexDone.push(tempIndex)
+            }
             doneContainer.prepend(div1)
             div1.removeChild(deleteButton)
             div1.removeChild(doneButton)
@@ -66,7 +117,6 @@ const clickCallback = () => {
             delBtn.setAttribute('class', 'bg-white mx-auto ml-0')
             delBtn.innerHTML = ''
             div1.append(delBtn)
-            //saveToLocalStorage()
         })
         //delete button
         const deleteButton = document.createElement('button')
@@ -76,15 +126,14 @@ const clickCallback = () => {
         div1.append(deleteButton)
         //4)click button to delete task
         deleteButton.addEventListener('click', () => {
+            for(let k=0; k<localStorage.length; k++) {
+                const tempIndex = localStorage.key(k)
+                const tempValue = localStorage.getItem(tempIndex)
+                //console.log(tempValue)
+                if(tempValue == p1.innerHTML) localStorage.removeItem(tempIndex)
+            }
             postContainer.removeChild(div1)
-            //saveToLocalStorage()
+
         })
-
-
-        
     } 
 }
-/*const saveToLocalStorage = () => {
-    localStorage.setItem('postContainer', postContainer.innerHTML)
-    localStorage.setItem('doneContainer', doneContainer.innerHTML)
-}*/
